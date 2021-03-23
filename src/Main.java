c703462494a261ce9ec564005e524a248ec17adf
1mbir <76110508+1mbir@users.noreply.github.com>
2021-03-23 17:52:36 +0100

    Dodanie wyświetlania własnej roli (#5)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Spaghetti\nrola: operations\ngithub ID: majewskimateusz");
+        System.out.println("Spaghetti\nrola: operations, github ID: majewskimateusz");
+        Info.print("developer", "1mbir");
         System.out.println("Spaghetti\nrola: operations\ngithub ID: majewskimateusz");
         System.out.println("Spaghetti\nrola: UI \ngithub ID: Mary9926");
         System.out.println("rola: tester\ngithub ID: kmieciakm");
-
     }
 }
