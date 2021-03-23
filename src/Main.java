aa9786dda7a8d265b6ba13b4c9faff5405eec891
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-03-23 17:39:49 +0100

    Added personal information (#3)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Spaghetti\nrola: operations\ngithub ID: majewskimateusz");
+        System.out.println("rola: tester\ngithub ID: kmieciakm");
     }
 }
