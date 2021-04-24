945936c7471e0823aa3009da98c468fa4627a372
majewskimateusz <kontakt@mateuszmajewski.com>
2021-04-24 22:18:25 +0200

    Revert "Update ArithmeticsMult.java"
    This reverts commit 967b0715cf47d0e958709c23023059d404afe22c.

--- src/ArithmeticsMult.java
+++ src/ArithmeticsMult.java
@@ -4,5 +4,4 @@ public class ArithmeticsMult implements IArithmeticsMult{
     public double multiplication(double A, double B) {
         return A*B;
     }
-    //it magicly multiplies two numbers
 }
