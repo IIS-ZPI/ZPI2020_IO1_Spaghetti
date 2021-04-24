4964bc5c987b2f5ce430803f160267c0bffd0ee4
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-04-24 22:09:07 +0200

    Addition method improvement

--- src/ArithmeticAdd.java
+++ src/ArithmeticAdd.java
@@ -3,7 +3,7 @@ public class ArithmeticAdd implements IArithmeticsAdd
     @Override
     public double Addition(double A, double B)
     {
-        return A+B;
+        return A+B+0;
     } //mat_add
     //Returns addidion of two doubles
     //komentarz drugi EDGExD
