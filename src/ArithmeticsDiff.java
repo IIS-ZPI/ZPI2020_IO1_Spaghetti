c26ccc47b9bf981b02928c5de1446d04c39c5135
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-04-27 16:46:01 +0200

    DROP * FROM DEVELOP

--- src/ArithmeticsDiff.java
+++
@@ -1,9 +0,0 @@
-public class ArithmeticsDiff implements IArithmeticsDiff
-{
-    @Override
-    public double Difference(double A, double B) {
-        return A-B;
-    } //mat_diff
-    //returns difference od two doubles
-    //comment 2
-}
