06904153d4150e7ab3547adac4e2582a0a48f05c
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-05-19 20:14:46 +0200

    Added skeleton of simple UI

--- api/main.py
+++ api/main.py
@@ -2,29 +2,9 @@ import json
 from urllib.request import urlopen
 from json_management import JsonManager, read_from_url
 from currency_management import *
-from matplotlib import pyplot as plt
-import tkinter
+from Window import *
 
 if __name__ == '__main__':
-    root = tkinter.Tk()
-    root.title('NPB')
-    root.geometry('500x500')
-
-    def plot_statistical_analysis():
-        cm = CurrencyManager('gbp', Period.ONE_MONTH)
-        tab = cm.get_array_from_period()
-        plt.plot(tab)
-        plt.show()
-        print('test')
-
-    mainMenu = tkinter.Menu()
-    root.config(menu=mainMenu)
-    currencyMenu = tkinter.Menu(mainMenu)
-    mainMenu.add_cascade(label='Select currency', menu=currencyMenu)
-    currencyMenu.add_command(label='GBP')
-    buttonPeriodMonth = tkinter.Button(root, text="One Month", command=plot_statistical_analysis)
-    buttonPeriodMonth.pack()
-    root.mainloop()
 
     cm = CurrencyManager('gbp', Period.ONE_MONTH)
     tab = cm.get_array_from_period()
