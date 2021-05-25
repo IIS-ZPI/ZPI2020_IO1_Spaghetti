59d29d95b4b8baae0b218239df9a600883e6da27
EDGExD <76109839+EDGExD@users.noreply.github.com>
2021-05-25 13:42:48 +0200

    Dodanie rozkładu zmian kwartalnych i mieisęcznych wybranych 2 walut

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ import json
 from urllib.request import urlopen
 from json_management import JsonManager, read_from_url
 from currency_management import *
+from distribution_of_changes import *
 
 
 
@@ -10,3 +11,8 @@ if __name__ == '__main__':
     tab = cm.get_array_from_period()
     print(tab)
     print(coefficient_of_variation(tab))
+
+    dc = DistributionChanges('usd', 'eur', Period.ONE_MONTH)
+    tab = dc.get_array_from_period()
+    tab2 = dc.count_changes_percentage(tab)
+    print(tab2)
