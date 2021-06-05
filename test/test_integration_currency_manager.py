98bef085dd4d69865045b3e9f8374dc6181598bf
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-06-05 18:29:17 +0200

    [Tests] Integration tests with NBP Api

--- test/test_integration_currency_manager.py
+++ test/test_integration_currency_manager.py
@@ -1,7 +1,30 @@
+import pytest
+
 from api.currency_management import *
+from urllib.error import HTTPError
 
 cm = CurrencyManager()
 
 
 def test_passes():
     assert 1 == 1
+
+
+def test_get_available_currencies():
+    currencies = cm.get_available_currencies()
+    assert any(currencies)
+
+
+def test_get_array_from_period_correct_1():
+    data = cm.get_array_from_period("usd", Period.ONE_WEEK)
+    assert any(data)
+
+
+def test_get_array_from_period_correct_2():
+    data = cm.get_array_from_period("ars", Period.ONE_MONTH)
+    assert any(data)
+
+
+def test_get_array_from_period_incorrect():
+    with pytest.raises(HTTPError):
+        cm.get_array_from_period("xxx", Period.ONE_WEEK)
--- test/test_unit_currency_manager.py
+++ test/test_unit_currency_manager.py
@@ -20,3 +20,11 @@ def test_count_raises():
 def test_count_no_change():
     no_change = cm.count_no_change([12, 14, 13, 11, 12, 11, 11, 12, 10])
     assert 1 == no_change
+
+
+def test_find_dates():
+    dates = cm.find_dates(Period.ONE_WEEK)
+    first_day = date.today().strftime('%Y-%m-%d')
+    last_day = (date.today() - timedelta(7)).strftime('%Y-%m-%d')
+    assert dates[0] == first_day
+    assert dates[1] == last_day
